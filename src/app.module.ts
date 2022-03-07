432bc2155b9a338063ee905e4e4f1ea99e05965f
Bryan Torres <bryan_torres@outlook.com>
2022-03-07 09:47:24 -0500

    Agregando variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { QuestionsModule } from './questions/questions.module';
 import { ProfilesModule } from './profiles/profiles.module';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb://AntiFraudesUser:YZs1lF8wTRuWVAyVPv4uNzic0lwKbg5r@35.193.35.251:17027/testSimulator?authSource=admin',
     ),
+    ConfigModule.forRoot(),
     QuestionsModule,
     ProfilesModule,
     UsersModule,
